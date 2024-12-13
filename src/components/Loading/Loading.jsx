1bb3dcf4548a6efedbca818f8895f547cb0d0215
Joel Baiju <joelbaiju98@gmail.com>
2024-12-14 00:40:03 +0530

    Your commit message

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,10 +4,17 @@ import './Loading.css'
 function Loading() {
     return (
         <div className='w-screen h-screen bg-black/30 backdrop-blur-sm flex justify-center items-center absolute z-50'>
-            <div className="loader">
-                <div className="box1"></div>
-                <div className="box2"></div>
-                <div className="box3"></div>
+            <div class="spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
     )
